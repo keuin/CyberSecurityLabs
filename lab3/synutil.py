1d48b18fb0bc9e080aa80352dc10ac34999be336
Keuin <keuinx@gmail.com>
2021-04-07 14:56:38 +0800

    update lab3 randomize ip segment id; improve code

--- lab3/synutil.py
+++ lab3/synutil.py
@@ -1,35 +1,35 @@
+import os
 import socket
 import struct
-from util import *
 
 
 def make_ip_segment(src: str, dst: str, layer4_data: bytes) -> bytes:
     ihl = 5
     version = 4
     tos = 0
-    tot_len = 20 + len(layer4_data)  # python seems to correctly fill the total length, dont know how ??
-    id = 54321  # Id of this packet
+    tot_len = 20 + len(layer4_data)
+    id_ = int.from_bytes(os.urandom(2), byteorder='little', signed=False)  # Id of this packet
     frag_off = 0
     ttl = 255
     protocol = socket.IPPROTO_TCP
-    check = 0  # python seems to correctly fill the checksum
-    saddr = socket.inet_aton(src)  # Spoof the source ip address if you want to
+    check = 0  # let the socket calculate
+    saddr = socket.inet_aton(src)
     daddr = socket.inet_aton(dst)
 
     ihl_version = (version << 4) + ihl
 
     # the ! in the pack format string means network order
-    return struct.pack('!BBHHHBBH4s4s', ihl_version, tos, tot_len, id,
+    return struct.pack('!BBHHHBBH4s4s', ihl_version, tos, tot_len, id_,
                        frag_off, ttl, protocol, check, saddr, daddr) + layer4_data
 
 
 def tcp_syn(dst_port: int) -> bytes:
     # tcp header fields
-    source = 1234  # source port
-    dest = dst_port  # destination port
+    sport = 1234  # source port
+    dport = dst_port  # destination port
     seq = 0
     ack_seq = 0
-    doff = 5  # 4 bit field, size of tcp header, 5 * 4 = 20 bytes
+    doff = 5
     # tcp flags
     fin = 0
     syn = 1
@@ -45,7 +45,7 @@ def tcp_syn(dst_port: int) -> bytes:
     tcp_flags = fin + (syn << 1) + (rst << 2) + (psh << 3) + (ack << 4) + (urg << 5)
 
     # the ! in the pack format string means network order
-    return struct.pack('!HHLLBBHHH', source, dest, seq, ack_seq, offset_res, tcp_flags, window, check, urg_ptr)
+    return struct.pack('!HHLLBBHHH', sport, dport, seq, ack_seq, offset_res, tcp_flags, window, check, urg_ptr)
 
 
 def send_syn(s: socket.socket, src: str, dst: str, dst_port: int):
